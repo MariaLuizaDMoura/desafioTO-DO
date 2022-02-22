0819a96b1c4bce1efd3ed65d9337609ff05fef0a
MariaLuizaDMoura <malumoura10@gmail.com>
2022-02-21 21:13:46 -0300

    @MariaLuizaDMoura [Task: To-do List | Spring - Criação do projeto no spring initialzr]

---
+++ todolistviceri/src/main/java/com/malucode/todolistviceri/TodolistviceriApplication.java
@@ -0,0 +1,13 @@
+package com.malucode.todolistviceri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistviceriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistviceriApplication.class, args);
+	}
+
+}
---
+++ todolistviceri/src/test/java/com/malucode/todolistviceri/TodolistviceriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malucode.todolistviceri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistviceriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
